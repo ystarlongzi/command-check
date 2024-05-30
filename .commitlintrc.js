ae9f93c4506844285dc5d1b0b73556235c789799
流采 <zhangzilong.zzl@163.com>
2024-05-30 09:57:33 +0800

    feat: init

---
+++ .commitlintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  // see：https://commitlint.js.org/#/reference-rules?id=rules
+  rules: {},
+};
---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'semi': ['error', 'always'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+};
